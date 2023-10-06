51d3e2085c4caf1e8ccf6c47c9737d6f31346339
Casandra Normand <casandra.normand@outlook.com>
2023-10-05 22:23:22 -0400

    implemented and styles PhotoListItem

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,37 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
+const props = {
+  id: "1",
+  location: {
+    city: "Montreal",
+    country: "Canada",
+  },
+  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+  username: "Joe Example",
+  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+};
+
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+  
+  const photos = new Array(3);
+  for(let i = 0; i < photos.length; i++){
+    photos[i] = <PhotoListItem
+    key={i}
+    imageSource={props.imageSource}
+    profile={props.profile}
+    username={props.username}
+    location={props.location}
+  />;
+  }
+
   return (
-    <div className="App">
-      <PhotoListItem/>
-    </div>
-  );
+  <div className="App">
+    {photos}
+  </div>
+);
 };
 
 export default App;
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,21 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import 'styles/PhotoListItem.scss';
+import PhotoFavButton from "components/PhotoFavButton";
 
-import "../styles/PhotoListItem.scss";
+const PhotoListItem = (props) => {
+  const [like, setLike] = useState('no');
+  const notLiked = (like === 'no') ? 'notLiked' : '';
+  
+  const switchLike = () => {
+    setLike((prevLike) => (prevLike === "yes") ? "no" : "yes");
+  };
 
+  const { imageSource, profile, username, location } = props;
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
-  /* Insert React */
+  return (
+    <section className={`photo-list__item ${notLiked}`}>
+      <PhotoFavButton like={like} switchLike={switchLike} />
+      <img className="photo-list__image" src={imageSource} alt="Posted" />
+      <div className="photo-list__user-profile">
+        <img className="photo-list__user-image" src={profile} alt="Profile" />
+        <div className="photo-list__user-details">
+          <div className="photo-list__user-info">
+            <p>{username}</p>
+            <p className="photo-list__user-location">{location.city}, {location.country}</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default PhotoListItem;
