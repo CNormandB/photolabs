4a061f770197bd76d0a4d1282c4316bda7d30cc7
Casandra Normand <casandra.normand@outlook.com>
2024-02-23 19:13:54 -0500

    Fave badge turns red when a photo is liked

--- frontend/src/components/FavBadge.jsx
+++ frontend/src/components/FavBadge.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import FavIcon from './FavIcon';
-
 import '../styles/FavBadge.scss';
 
 const FavBadge = ({ isFavPhotoExist, toggleLikedPhotos }) => {
 
   return (
     <button onClick={toggleLikedPhotos} className='fav-badge' >
-      <FavIcon displayAlert={isFavPhotoExist}/>
+      <FavIcon displayAlert={isFavPhotoExist} selected={isFavPhotoExist}/>
     </button>
   ) 
 };
